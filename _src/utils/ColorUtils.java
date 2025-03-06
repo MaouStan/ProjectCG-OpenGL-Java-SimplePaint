d64f1b50fb46bfa0750f2609ce4b89b56c9eb34d
MaouStan <zazalnm4@gmail.com>
2025-03-06 14:10:58 +0700

    t

--- _src/utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.awt.Color;
-
-public class ColorUtils {
-    public static Color getRandomColor() {
-        return new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
-    }
-}
